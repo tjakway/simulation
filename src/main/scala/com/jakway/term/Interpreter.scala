5cd55d69857bf2afc2509490cf43f05e42505a51
Thomas Jakway <tjakway@nyu.edu>
2018-06-05 10:14:09 -0700

    began writing the interpreter

--- src/main/scala/com/jakway/term/Interpreter.scala
+++ src/main/scala/com/jakway/term/Interpreter.scala
@@ -1,6 +1,23 @@
 package com.jakway.term
 
-import com.jakway.term.numeric.types.NumericType
+import com.jakway.term.numeric.types.{NumericType, SimError}
+
+object Interpreter {
+  type SymbolTable = Map[String, Term]
+
+
+  class InterpreterError(override val msg: String,
+                         val table: SymbolTable,
+                         val term: Term)
+    extends SimError(s"Error occurred in interpretation" +
+      s" of term < $term > with symbol table < $table >: $msg")
+
+  class SymbolNotFoundError[N <: NumericType[M], M]
+                           (val variable: Variable[N, M],
+                            override val table: SymbolTable,
+                            override val term: Term)
+    extends InterpreterError(variable.toString, table, term)
+}
 
 /**
   *
@@ -10,7 +27,23 @@ import com.jakway.term.numeric.types.NumericType
   * @tparam M
   */
 class Interpreter[N <: NumericType[M], M](n: NumericType[M]) {
-  def eval(t: Term) = t match {
+  import Interpreter._
+
+  def eval(table: SymbolTable)(t: Term): Either[SimError, Term] = t match {
+    case Literal(x) => n.readLiteral(x).map(Raw.apply)
+
+    case v@Variable(name, _) => table.get(name) match {
+      case Some(t) => eval(table)(t)
+      case None => Left(new SymbolNotFoundError(v, table, t))
+    }
+
     case _ => ???
   }
+
+  /**
+    * type for values we've looked up in the current evaluation context
+    * @param value
+    */
+  case class Raw(value: M) extends NumericTerm[N, M]
 }
+
