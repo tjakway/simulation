ce3d475cf5cfacbc2fe3a928df4edb8d587d934b
Thomas Jakway <tjakway@nyu.edu>
2018-10-10 23:52:32 -0400

    converted mapAll tests to a class

--- src/test/scala/com/jakway/term/test/TestHasSubterms.scala
+++ src/test/scala/com/jakway/term/test/TestHasSubterms.scala
@@ -17,19 +17,31 @@ object TestHasSubterms {
 class TestHasSubterms extends FlatSpec with Matchers {
   import TestHasSubterms._
 
+  val asserter = this
+
+  class MapAllTest(
+    val description: String,
+    val expected: Term,
+    val input: Term,
+    val function: Term => Term) {
+
+    "mapAll" should description in {
+      TermOperations.mapAll(input)(function) shouldEqual expected
+    }
+  }
+
+  new MapAllTest(
+    description = "let literals be replaced",
+    expected = Add(Literal("50"), Literal("20")),
+    input = addTwoLiterals,
+    function = t => t match {
+        case Literal(x) => Literal(x + "0")
+        case x => x
+      }
+  )
+
   "mapAll" should "apply id correctly" in {
     allExpressions.foreach(
       x => TermOperations.mapAll(x)(id) shouldEqual x)
-  }
-
-  it should "let literals be replaced" in {
-    val expected = Add(Literal("50"), Literal("20"))
-
-    def addAZero(t: Term): Term = t match {
-      case Literal(x) => Literal(x + "0")
-      case x => x
-    }
-
-    TermOperations.mapAll(addTwoLiterals)(addAZero) shouldEqual expected
   }
 }
