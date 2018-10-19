7951324c2a2e9c8d6c97248358a1071ccc2f1300
Thomas Jakway <tjakway@nyu.edu>
2018-10-19 12:50:06 -0400

    added skeleton warnings framework

---
+++ src/main/scala/com/jakway/term/interpreter/warn/Warning.scala
@@ -0,0 +1,64 @@
+package com.jakway.term.interpreter.warn
+
+import java.util.Locale
+
+import com.jakway.term.numeric.types.SimError
+import com.jakway.term.{Equation, Term}
+
+
+/**
+  * Type that wraps things we warn about
+  */
+sealed trait WarningObject {
+  def warningFor: String
+}
+case class TermWarning(t: Term) extends WarningObject {
+  override def warningFor: String = t.toString
+}
+case class EquationWarning(eq: Equation) extends WarningObject {
+  override def warningFor: String = eq.toString
+}
+
+class Warning(val obj: WarningObject, val warning: String)
+  extends SimError(
+    s"Warning for ${obj.warningFor}: $warning") {
+
+  /**
+    * convenience constructors
+    *
+    * @param t
+    * @param warning
+    * @return
+    */
+  def this(t: Term, warning: String) =
+    this(TermWarning(t), warning)
+
+  def this(eq: Equation, warning: String) =
+    this(EquationWarning(eq), warning)
+
+}
+
+object Warning {
+  /**
+    * print warnings grouped by object
+    * @param warnings
+    * @return
+    */
+  def formatWarnings(warnings: Seq[Warning]): String = {
+    import java.util.Formatter
+    val sb: StringBuffer = new StringBuffer()
+    val formatter: Formatter = new Formatter(sb, Locale.getDefault())
+
+    warnings.groupBy(_.obj).foreach {
+      case (thisTerm, termWarnings) => {
+        //note: java.util.Formatter converts \n to the platform-
+        //specific line separator
+        formatter.format("Warnings for %s:\n", thisTerm.warningFor)
+        termWarnings.foreach(w =>
+          formatter.format("\t%s\n", w))
+      }
+    }
+
+    sb.toString
+  }
+}
