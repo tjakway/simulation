787a724e2d23aafb39bb3d015fb567169b6847f8
Thomas Jakway <tjakway@nyu.edu>
2018-10-19 12:50:06 -0400

    added skeleton warnings framework

---
+++ src/main/scala/com/jakway/term/interpreter/Checks.scala
@@ -0,0 +1,39 @@
+package com.jakway.term.interpreter
+
+import com.jakway.term.interpreter.warn.Warning
+import com.jakway.term.{Equation, Term}
+import com.jakway.term.numeric.types.SimError
+
+
+object Checks {
+  private type Check[T] = T => Seq[Warning]
+  private trait HasChecks[T] {
+    val checks: Seq[T => Seq[Warning]]
+  }
+
+  private def foldChecks[T](on: T,
+                    checks: Seq[T => Seq[Warning]]):
+    Seq[Warning] = {
+
+    checks.foldLeft(Seq(): Seq[Warning]) {
+      case (acc, thisCheck) => acc ++ thisCheck(on)
+    }
+  }
+
+  private def foldChecks[T](on: T,
+                    hasChecks: HasChecks[T]): Seq[Warning] =
+    foldChecks(on, hasChecks.checks)
+
+  private object EquationChecks extends HasChecks[Equation] {
+    //TODO
+    def checkNoVariables(eq: Equation): Seq[Warning] = ???
+
+    val checks: Seq[Equation => Seq[Warning]] = Seq(
+      checkNoVariables
+    )
+  }
+
+  def checkEquation(eq: Equation): Seq[Warning] = {
+    foldChecks(eq, EquationChecks)
+  }
+}
