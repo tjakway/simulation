83965c9931a0002d05281f502fef7d4fd00f201a
Thomas Jakway <tjakway@nyu.edu>
2018-10-20 15:51:37 -0400

    fixed type comparison in findVariables

--- src/test/scala/com/jakway/term/test/TestFindVariables.scala
+++ src/test/scala/com/jakway/term/test/TestFindVariables.scala
@@ -1,14 +1,19 @@
 package com.jakway.term.test
 
-import com.jakway.term.TermOperations
+import scala.reflect.runtime.universe._
+import com.jakway.term.{TermOperations, Variable}
 import com.jakway.term.numeric.types.NumericType
 import org.scalatest.{FlatSpec, Matchers}
 
 abstract class TestFindVariables[N <: NumericType[M], M]
   (override val numericType: N)
+  (implicit ev: TypeTag[Variable[N, M]])
   extends FlatSpec with Matchers with NumericTypeTest[N, M] {
 
+  val expr = new Expressions[N, M]()
+
   "findVariables" should "return Seq() for literals" in {
-    //TermOperations.findVariables[N, M]()
+    TermOperations
+      .findVariables[N, M](expr.addTwoLiterals) shouldEqual Seq()
   }
 }
