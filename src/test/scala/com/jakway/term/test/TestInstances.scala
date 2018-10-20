4a7db71e4084660e47d44087618d31d1275f0fbe
Thomas Jakway <tjakway@nyu.edu>
2018-10-20 15:28:03 -0400

    fixup! finished parameterized type framework

--- src/test/scala/com/jakway/term/test/TestInstances.scala
+++ src/test/scala/com/jakway/term/test/TestInstances.scala
@@ -3,6 +3,10 @@ package com.jakway.term.test
 import com.jakway.term.numeric.types.NumericType
 import com.jakway.term.numeric.types.implementations.DoublePrecision
 
+/**
+  * TODO: not sure if these tests should extend NumericTypeTest, or if
+  * NumericTypeTest is even necessary...
+  */
 package double {
   object Double {
     type M = Double
