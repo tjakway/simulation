a269566241b40b79242fbb3ae7c1bb843e97d3a8
Thomas Jakway <tjakway@nyu.edu>
2018-10-20 20:21:08 -0400

    moved TermOperations into its own file

--- src/main/scala/com/jakway/term/Rewriter.scala
+++ src/main/scala/com/jakway/term/Rewriter.scala
@@ -1,11 +1,6 @@
 package com.jakway.term
 
-import scala.reflect.runtime.universe._
 import com.jakway.term.numeric.types.{NumericType, SimError}
-import org.slf4j.{Logger, LoggerFactory}
-
-import scala.annotation.tailrec
-import scala.util.{Failure, Success, Try}
 
 class Rewriter {
   case class TermNotFoundError(refactorFor: Term, equation: Equation)
@@ -28,78 +23,7 @@ class Rewriter {
   }
 }
 
-object TermOperations {
-  case class CastHasSubtermsError(castTerm: Term, t: Throwable)
-    extends SimError(s"Error casting $castTerm to an instance of HasSubterms",
-      t)
 
-  val logger: Logger = LoggerFactory.getLogger(getClass())
-
-  /**
-    * *** XXX WARNING: *** it is the caller's responsibility to ensure that
-    * mapped types conform to the expectation of enclosing terms
-    * @param t
-    * @param f
-    * @return
-    */
-  def mapAll(t: Term)(f: Term => Term): Term = t match {
-    case x: HasSubterms => f(x.newInstance(x.subterms.map(f)))
-    case x => f(x)
-  }
-
-  def foreach(t: Term)(f: Term => Unit): Unit =
-    mapAll(t) { a =>
-      f(a)
-      a
-    }
-
-  def replaceSubterms(replace: HasSubterms => Seq[Term] => Seq[Term])
-                     (h: HasSubterms): Either[SimError, HasSubterms] = {
-    Try {
-      h.newInstance(replace(h)(h.subterms)).asInstanceOf[HasSubterms]
-    } match {
-      case Success(x) => Right(x)
-      case Failure(e) if e.isInstanceOf[ClassCastException] => {
-        Left(CastHasSubtermsError(h, e))
-      }
-      case Failure(x) => Left(new SimError(x))
-    }
-  }
-
-  def foldSubterms[B](t: Term)(z: B)(f: (B, Term) => B): B = {
-    t match {
-      //fold over the top level term then subterms
-      case h: HasSubterms => {
-        val start = f(z, h)
-        h.subterms.foldLeft(start) {
-          case (acc, thisTerm) => foldSubterms(thisTerm)(acc)(f)
-        }
-      }
-      case _ => f(z, t)
-    }
-  }
-
-  def findVariables[N <: NumericType[M], M](t: Term)
-    : Seq[Variable[N, M]] = {
-
-    val empty: Seq[Variable[N, M]] = Seq()
-
-    //accumulate variables
-    foldSubterms(t)(empty) {
-      case (variables, x) => {
-
-        //need to check reified types to get around type erasure
-        if(x.isInstanceOf[Variable[N @unchecked, M @unchecked]]) {
-          variables :+ x.asInstanceOf[Variable[N, M]]
-        }
-        else {
-          variables
-        }
-      }
-    }
-  }
-
-}
 
 class Simplifier {
   def simplifiers[A <: Term]: Set[A => Either[SimError, Term]] = ??? //TODO
