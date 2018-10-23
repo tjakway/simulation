ecdb447dad4fea7e263a8d3b1563608f9b9919dc
Thomas Jakway <tjakway@nyu.edu>
2018-10-23 16:59:08 -0400

    wrote SubstituteFunction.applyFunctions

--- src/main/scala/com/jakway/term/solver/SubstituteFunction.scala
+++ src/main/scala/com/jakway/term/solver/SubstituteFunction.scala
@@ -3,6 +3,7 @@ package com.jakway.term.solver
 import com.jakway.term.interpreter.warn.Warning
 import com.jakway.term.{Equation, HasSubterms, Term, TermOperations}
 import com.jakway.term.numeric.types.SimError
+import com.jakway.term.solver.SubstituteFunction.Applications.ApplicationType
 
 import scala.util.{Failure, Success, Try}
 
@@ -40,9 +41,12 @@ object SubstituteFunction {
     Seq(ApplyToTerm(toReplace, newTerm), ApplyToEquation(applyToEquation))
   }
 
-  class Applications(val applications: Seq[(SubstituteFunction, Equation, Seq[Warning])],
+  class Applications(val applications: Seq[Applications.ApplicationType],
                      val start: Equation,
                      val result: Equation)
+  object Applications {
+    type ApplicationType = (SubstituteFunction, Equation, Seq[Warning])
+  }
 
   class SubstituteFunctionError(override val msg: String)
     extends SimError(msg) {
@@ -73,12 +77,15 @@ object SubstituteFunction {
     extends SubstituteFunctionError(f, e, s"exception thrown while processing " +
       s"substitute function: $t")
 
+  case class SubstituteFunctionErrors(errors: Seq[SimError])
+    extends SimError(s"Substitute Function errors: $errors")
 
-  def applyFunctions(fs: Seq[SubstituteFunction], orig: Equation):
+
+  def applyFunctions(fs: Seq[SubstituteFunction], origEquation: Equation):
   Either[SimError, Applications] = {
 
-    def thisApplication(f: SubstituteFunction, to: Equation):
-    Either[SimError, (Equation, Seq[Warning])] = Try {
+    def applyThisFunction(f: SubstituteFunction, to: Equation):
+    Either[SimError, ApplicationType] = Try {
 
       var matchingTerms: Seq[Term] = Seq()
       val resultingEquation: Equation = f match {
@@ -120,7 +127,7 @@ object SubstituteFunction {
 
       //return the result of the function application and
       // any warnings we've gathered from it
-      (resultingEquation, warnings)
+      (f, resultingEquation, warnings)
     } match {
       case Success(x) => Right(x)
       case Failure(e) if e.isInstanceOf[SimError] =>
@@ -129,6 +136,32 @@ object SubstituteFunction {
     }
 
 
+    //TODO: could add an Equation to Left and keep running after errors (to accumulate
+    //more errors)
+    //Left: accumulate errors
+    //Right: accumulate results and track the equation to apply the next function to
+    val empty: Either[Seq[SimError], (Seq[Applications.ApplicationType], Equation)] =
+      Right(Seq(), origEquation)
+    fs.foldLeft(empty) {
+      //accumulate function applications in the Either type
+      //and stop if we get a Left
+      case (Right((acc, thisEquation)), thisFunction) =>
+        applyThisFunction(thisFunction, thisEquation) match {
+          case Right(res) => Right(acc :+ res, res._2)
+          case Left(res) => Left(Seq(res))
+        }
+
+      //don't continue on error
+      case (errs@Left(_), _) => errs
+    }.map {
+      //wrap the result and return
+      case (applications, finalEquation) =>
+        new Applications(applications, origEquation, finalEquation)
+      } match {
+        //flatten the Seq[SimError] by wrapping it in a new type
+        case Left(errs) => Left(SubstituteFunctionErrors(errs))
+        case Right(x) => Right(x)
+      }
 
   }
 }
