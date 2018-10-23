f4ab1e4a51332c4922c0648ee821716efbd514a2
Thomas Jakway <tjakway@nyu.edu>
2018-10-23 16:14:45 -0400

    implemented substitute function application to replace the original variable with identity

--- src/main/scala/com/jakway/term/Solver.scala
+++ src/main/scala/com/jakway/term/Solver.scala
@@ -1,6 +1,9 @@
 package com.jakway.term
 
+import com.jakway.term.interpreter.warn.Warning
 import com.jakway.term.numeric.types.{NumericType, SimError}
+
+import scala.util.{Failure, Success, Try}
 
 class Solver[N <: NumericType[M], M] {
   case class TermNotFoundError(refactorFor: Term, equation: Equation)
@@ -81,7 +84,91 @@ class Solver[N <: NumericType[M], M] {
       Seq(ApplyToTerm(toReplace, newTerm), ApplyToEquation(applyToEquation))
     }
 
-    def applyFunctions(to: Equation, fs: Seq[SubstituteFunction]) = {
+    class Applications(val applications: Seq[(SubstituteFunction, Equation, Seq[Warning])],
+                       val start: Equation,
+                       val result: Equation)
+
+    class SubstituteFunctionError(override val msg: String)
+      extends SimError(msg) {
+      def this(f: SubstituteFunction, e: Equation, msg: String) =
+        this(s"Error while processing $f in $e: $msg")
+    }
+
+    case class MultipleTermsMatchSubstitutionFunction(f: SubstituteFunction,
+                                                      in: Equation,
+                                                      matchingTerms: Seq[Term])
+      extends Warning(in, "Multiple terms matched substitution function" +
+        s" $f in equation $in: $matchingTerms")
+
+    class CouldNotFindTermError(f: ApplyToTerm,
+                              in: Equation)
+     extends SubstituteFunctionError(f, in,
+       s"Could not find term ${f.replaceTerm} in $in")
+
+    class ReturnedError(f: SubstituteFunction,
+                        e: Equation,
+                        res: SimError)
+      extends SubstituteFunctionError(f, e, s"substitute function returned " +
+        s"error result $res")
+
+    class UnknownError(f: SubstituteFunction,
+                       e: Equation,
+                       t: Throwable)
+      extends SubstituteFunctionError(f, e, s"exception thrown while processing " +
+        s"substitute function: $t")
+
+
+    def applyFunctions(fs: Seq[SubstituteFunction], orig: Equation):
+      Either[SimError, Applications] = {
+
+      def thisApplication(f: SubstituteFunction, to: Equation):
+        Either[SimError, (Equation, Seq[Warning])] = Try {
+
+        var matchingTerms: Seq[Term] = Seq()
+        val resultingEquation: Equation = f match {
+          case ApplyToTerm(replaceTerm, replaceWith) => {
+
+            //TODO: traversing the entire AST for each substitution function
+            //is very inefficient
+            val newLeftTerm = TermOperations.mapAll(to.left) { thisTerm =>
+              if(thisTerm == replaceTerm) {
+                val substitutePerformed = matchingTerms.length > 0
+
+                matchingTerms = matchingTerms :+ thisTerm
+                if(!substitutePerformed) {
+                  //TODO: refactor into something more elegant than
+                  //throwing the error here and catching it in the Try block
+                  replaceWith(thisTerm).right.get
+                } else {
+                  thisTerm
+                }
+              } else {
+                thisTerm
+              }
+            }
+
+            to.copy(left = newLeftTerm)
+          }
+          case ApplyToEquation(f) => ???
+        }
+
+        val warnings: Seq[Warning] =
+          if(matchingTerms.length > 1) {
+            Seq(MultipleTermsMatchSubstitutionFunction(f, to, matchingTerms))
+          } else {
+            Seq()
+          }
+
+        //return the result of the function application and
+        // any warnings we've gathered from it
+        (resultingEquation, warnings)
+      } match {
+        case Success(x) => Right(x)
+        case Failure(e) if e.isInstanceOf[SimError] =>
+          Left(new ReturnedError(f, to, e.asInstanceOf[SimError]))
+        case Failure(e) => Left(new UnknownError(f, to, e))
+      }
+
       fs.map { x => x match {
         case ApplyToTerm(replaceTerm, replaceWith) => {
           to.left
