bc63e9ad33a907d936350afc8d6be464d011a7e9
Thomas Jakway <tjakway@nyu.edu>
2018-10-24 12:01:15 -0400

    made Simplifier a trait

--- src/main/scala/com/jakway/term/simplifier/Simplifier.scala
+++ src/main/scala/com/jakway/term/simplifier/Simplifier.scala
@@ -3,37 +3,10 @@ package com.jakway.term.simplifier
 import com.jakway.term.numeric.types.{NumericType, SimError}
 import com.jakway.term.{BinaryNumericOperation, HasSubterms, Operation, Term}
 
-class Simplifier {
-  def simplifiers[A <: Term]: Set[A => Either[SimError, Term]] = ??? //TODO
+trait Simplifier {
+  def simplify(t: Term): Either[SimError, Term]
+}
 
-  /*
-  def mkSimplifier[A <: Term](f: A => Term): A => Either[SimError, Term] = {
-    (arg: )
-  }*/
-
-  def simplify[N <: NumericType[M], M](t: Term): Either[SimError, Term] = {
-    //if there are no subterms, it's already in simplest form
-    if(!t.isInstanceOf[HasSubterms]) {
-      Right(t)
-    } else {
-      t match {
-        case o: Operation => {
-          //remove outer term if it's an operation on identities
-          if (o.subterms.forall(_ == o.identity)) {
-            //TODO
-            // Right(o)
-            ???
-          } else {
-            ???
-          }
-        }
-        case b: BinaryNumericOperation[N, M] => {
-          ??? //TODO
-        }
-
-        case _ => ???
-      }
-    }
-  }
-
+object Simplifier {
+  val simplifiers: Set[Term => Either[SimError, Term]] = Set()
 }
