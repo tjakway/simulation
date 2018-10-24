1c41fa62ba0f03da9f2619b100e475236d6ba518
Thomas Jakway <tjakway@nyu.edu>
2018-10-24 12:03:26 -0400

    fixup! fixup! made Simplifier a trait

--- src/main/scala/com/jakway/term/simplifier/Simplifier.scala
+++ src/main/scala/com/jakway/term/simplifier/Simplifier.scala
@@ -4,7 +4,7 @@ import com.jakway.term.numeric.types.{NumericType, SimError}
 import com.jakway.term.{BinaryNumericOperation, HasSubterms, Operation, Term}
 
 trait Simplifier {
-  def simplify[N <: NumericType[M], M](t: Term): Either[SimError, Term]
+  def simplify(t: Term): Either[SimError, Term]
 }
 
 object Simplifier {
