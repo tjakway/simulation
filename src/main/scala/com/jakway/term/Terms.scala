473346a33055db392f411b559fb31bc885d75317
Thomas Jakway <tjakway@nyu.edu>
2018-10-29 10:49:05 -0400

    overrode Equation.equals to compare terms using equals

--- src/main/scala/com/jakway/term/Terms.scala
+++ src/main/scala/com/jakway/term/Terms.scala
@@ -528,6 +528,19 @@ case class Equation(left: Term, right: Term) {
 
   def contains(t: Term): Boolean =
     left.contains(t) || right.contains(t)
+
+  /**
+    * compares terms using .matches
+    * @param o
+    * @return
+    */
+  override def equals(o: Any): Boolean = {
+    def eq(e: Equation): Boolean = {
+      left.matches(e.left) && right.matches(e.right)
+    }
+
+    o.isInstanceOf[Any] && eq(o.asInstanceOf[Equation])
+  }
 }
 
 class Examples[N <: NumericType[M], M] {
