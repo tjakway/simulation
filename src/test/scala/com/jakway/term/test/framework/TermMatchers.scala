531041171d78585c1b35a1564f67af141e7a7a20
Thomas Jakway <tjakway@nyu.edu>
2018-10-29 10:43:44 -0400

    added sanity check to MatchesTerm

--- src/test/scala/com/jakway/term/test/framework/TermMatchers.scala
+++ src/test/scala/com/jakway/term/test/framework/TermMatchers.scala
@@ -1,6 +1,7 @@
 package com.jakway.term.test.framework
 
 import com.jakway.term.Term
+import com.jakway.term.numeric.types.SimError
 import org.scalatest.matchers.{MatchResult, Matcher}
 
 trait TermMatchers {
@@ -10,9 +11,37 @@ trait TermMatchers {
       def mkMsg(msg: String): String =
         s"$left $msg .match() $expected"
 
+      //sanity check
+      checkMatchEachOther(left, expected)
+
       MatchResult(left.matches(expected),
         mkMsg("did not"), mkMsg("did"))
     }
+
+    /**
+      * if a.matches(b) then b.matches(a) should return true
+      * and vice-versa
+      */
+    def checkMatchEachOther(a: Term, b: Term) = {
+      if(a.matches(b)) {
+        if(!b.matches(a)) {
+          throw MatchesTermError(s"a.matches(b) returned true" +
+            s" but b.matches(a) returned false" +
+            s" for a=$a, b=$b")
+        }
+      }
+
+      if(b.matches(a)) {
+        if(!a.matches(b)) {
+          throw MatchesTermError(s"b.matches(a) returned true" +
+            s" but a.matches(b) returned false" +
+            s" for a=$a, b=$b")
+        }
+      }
+    }
+
+    case class MatchesTermError(override val msg: String)
+      extends SimError(msg)
   }
 
   def shouldMatchTerm(expectedTerm: Term) = new MatchesTerm(expectedTerm)
