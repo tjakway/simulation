4dc0f7a3fd0eddd37afe6774c9f25707b10a4225
Thomas Jakway <tjakway@nyu.edu>
2018-10-30 12:50:14 -0400

    refactored eval helpers into their own classes as subtypes of EvalHelper

--- src/main/scala/com/jakway/term/interpreter/Eval.scala
+++ src/main/scala/com/jakway/term/interpreter/Eval.scala
@@ -25,20 +25,27 @@ case class Raw[N <: NumericType[M], M](value: M) extends NumericTerm[N, M] {
   * @tparam N
   * @tparam M
   */
-class Eval[N <: NumericType[M], M](n: NumericType[M])
+class Eval[N <: NumericType[M], M](val n: NumericType[M])
+                                  (val evalHelpers: EvalHelpers[N, M])
   extends Interpreter {
-
   import Interpreter._
 
+  def readLiteral(l: Literal[N, M]): Either[SimError, Raw[N, M]] = l match {
+    case Literal(value) => n.readLiteral(value).map(Raw.apply)
+  }
+
+
   def eval(table: SymbolTable)(t: Term): EvalType = t match {
-    case Literal(x) => n.readLiteral(x).map(Raw.apply)
+    case l: Literal[N, M] => readLiteral(l)
 
     case v@Variable(name, _) => table.get(name) match {
       case Some(t) => eval(table)(t)
       case None => Left(new SymbolNotFoundError(v, table, t))
     }
 
-    case f: FunctionCall[N, M] => EvalFunctionCall.evalFunctionCall[N, M](table)(this, f)
+    case Negative(arg) => eval(table)(Multiply(arg, Literal("-1")))
+
+    case f: FunctionCall[N, M] => evalHelpers.functionCall(table)(f)
 
     case _ => ???
   }
@@ -53,15 +60,46 @@ object Eval {
   def lookupTerm[N <: NumericType[M], M]
             (table: SymbolTable, variable: Variable[N, M]): Option[Term] =
     table.get(variable.name)
+
+  abstract class EvalHelper[A <: Term](val interpreter: Interpreter)
+  {
+    def apply(table: SymbolTable)(a: A): EvalType
+
+    def recurse(table: SymbolTable)(t: Term): EvalType =
+      interpreter.eval(table)(t)
+  }
 }
 
-object EvalFunctionCall {
+class EvalHelpers[N <: NumericType[M], M](
+     val negativeTerm: EvalNegative[N, M],
+     val functionCall: EvalFunctionCall[N, M]
+)
+
+object EvalHelpers {
+  def setup[N <: NumericType[M], M](
+           readLiteral: Literal[N, M] => Either[SimError, Raw[N, M]],
+           recurse: Interpreter,
+           table: SymbolTable): Either[SimError, EvalHelpers[N, M]] = {
+    for {
+      negativeOne <- readLiteral(Literal[N, M]("-1"))
+    } yield {
+      new EvalHelpers[N, M](
+        new EvalNegative[N, M](recurse, negativeOne),
+        new EvalFunctionCall[N, M](recurse))
+    }
+  }
+}
+
+
+class EvalFunctionCall[N <: NumericType[M], M]
+  (val recurse: Interpreter)
+  extends EvalHelper[FunctionCall[N, M]](recurse) {
+
   case class WrongNumberOfArgumentsError(override val msg: String)
     extends EvalError(msg)
 
-  def evalFunctionCall[N <: NumericType[M], M](table: SymbolTable)
-                                              (recurse: Interpreter,
-                                               f: FunctionCall[N, M]): EvalType
+  def apply(table: SymbolTable)
+           (f: FunctionCall[N, M]): EvalType
     = f match {
     case FunctionCall(function, args)
       if function.arity != args.length =>
@@ -87,4 +125,22 @@ object EvalFunctionCall {
       recurse.eval(newSymbolTable)(function.body)
     }
   }
+}
+
+/**
+  * replace Negative(x) with (x * -1) and eval
+  * @param negativeOne
+  * @tparam N
+  * @tparam M
+  */
+class EvalNegative[N <: NumericType[M], M](val recurse: Interpreter,
+                                           val negativeOne: Raw[N, M])
+  extends EvalHelper[Negative[N, M]](recurse) {
+
+  def replaceWith(neg: Negative[N, M]): Term =
+    Multiply(neg.arg, negativeOne)
+
+  def apply(t: SymbolTable)
+           (term: Negative[N, M]): EvalType =
+    recurse(t)(replaceWith(term))
 }
