e76206c7592544ba506962b7b3de51e8855c6b11
Thomas Jakway <tjakway@nyu.edu>
2018-10-30 13:30:13 -0400

    implemented special literals pi and e

--- src/main/scala/com/jakway/term/numeric/types/NumericType.scala
+++ src/main/scala/com/jakway/term/numeric/types/NumericType.scala
@@ -15,6 +15,69 @@ class SimError(val msg: String)
   }
 }
 
+/**
+  * literals that need special handling
+  * famous constants like e, pi, etc.
+  * these typically already have specific backing
+  * by the underling numeric type
+  */
+object SpecialLiterals {
+  class SpecialLiteral(val names: Set[String]) {
+    def this(name: String) = this(Set(name))
+
+    /**
+      * returns true if the argument is
+      * this SpecialLiteral or if it is any of the names
+      * of this SpecialLiteral
+      * @param o
+      * @return
+      */
+    override def equals(o: Any): Boolean = {
+      def eqString(s: String): Boolean = {
+        names.contains(s)
+      }
+
+      def eqOther(other: SpecialLiteral): Boolean =
+        names == other.names
+
+      ( o.isInstanceOf[String] &&
+        eqString(o.asInstanceOf[String]) ) ||
+        ( o.isInstanceOf[SpecialLiteral]
+            && eqOther(o.asInstanceOf[SpecialLiteral]))
+    }
+  }
+
+  val e = new SpecialLiteral("e")
+  val pi =
+    //see https://en.wikipedia.org/wiki/Pi_(letter)
+    //for unicode representations of pi
+    new SpecialLiteral(Set("pi",
+      //"\uD835\uDF0B" is unicode 'MATHEMATICAL ITALIC SMALL PI'
+      //https://www.fileformat.info/info/unicode/char/1d70b/index.htm
+      "\uD835\uDF0B",
+       //'MATHMETICAL SANS-SERIF BOLD SMALL PI'
+       "\uD835\uDF7F"))
+
+
+  /**
+    * @param lit
+    * @return true if this is a name of any special literal
+    */
+  def contains(lit: String): Boolean =
+    specialLiterals.find(_.names.contains(lit))
+                   .isDefined
+
+  val specialLiterals: Set[SpecialLiteral] = Set(
+    e,
+    pi
+ )
+
+  case class SpecialLiteralNotImplementedError(lit: String)
+    extends SimError(s"$lit was identified as a special literal" +
+      s" but its value was not properly assigned" +
+      s" (there was an error in a NumericType implementation)")
+}
+
 
 trait NumericType[M] {
   type UnaryFunction = M => Either[SimError, M]
