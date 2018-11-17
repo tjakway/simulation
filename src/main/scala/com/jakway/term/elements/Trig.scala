f461f918b32fcd2f67777a0fc8c899fd18cc5099
Thomas Jakway <tjakway@nyu.edu>
2018-11-16 23:14:13 -0800

    changed trig functions and OneArgumentFunction to take a NumericTerm instead of a Term as their argument

--- src/main/scala/com/jakway/term/elements/Trig.scala
+++ src/main/scala/com/jakway/term/elements/Trig.scala
@@ -6,37 +6,37 @@ import com.jakway.term.numeric.types.{NumericType, SimError}
 trait TrigFunction[N <: NumericType[M], M]
   extends OneArgumentFunction[N, M]
 
-case class Sin[N <: NumericType[M], M](override val argument: Term) extends TrigFunction[N, M] {
+case class Sin[N <: NumericType[M], M](override val argument: NumericTerm[N, M]) extends TrigFunction[N, M] {
   override def inverseConstructorE: Seq[Term] => Either[SimError, Term] =
     mkInverseConstructorE(Arcsin.apply)
 
   override def newInstance: NewInstanceF = mkNewInstance[Sin[N,M]](Sin.apply)
 }
-case class Cos[N <: NumericType[M], M](override val argument: Term) extends TrigFunction[N, M] {
+case class Cos[N <: NumericType[M], M](override val argument: NumericTerm[N, M]) extends TrigFunction[N, M] {
   override def inverseConstructorE: Seq[Term] => Either[SimError, Term] =
     mkInverseConstructorE(Arccos.apply)
 
   override def newInstance: NewInstanceF = mkNewInstance[Cos[N,M]](Cos.apply)
 }
-case class Tan[N <: NumericType[M], M](override val argument: Term) extends TrigFunction[N, M] {
+case class Tan[N <: NumericType[M], M](override val argument: NumericTerm[N, M]) extends TrigFunction[N, M] {
   override def inverseConstructorE: Seq[Term] => Either[SimError, Term] =
     mkInverseConstructorE(Arctan.apply)
 
   override def newInstance: NewInstanceF = mkNewInstance[Tan[N,M]](Tan.apply)
 }
-case class Arcsin[N <: NumericType[M], M](override val argument: Term) extends TrigFunction[N, M] {
+case class Arcsin[N <: NumericType[M], M](override val argument: NumericTerm[N, M]) extends TrigFunction[N, M] {
   override def inverseConstructorE: Seq[Term] => Either[SimError, Term] =
     mkInverseConstructorE(Sin.apply)
 
   override def newInstance: NewInstanceF = mkNewInstance[Arcsin[N,M]](Arcsin.apply)
 }
-case class Arccos[N <: NumericType[M], M](override val argument: Term) extends TrigFunction[N, M] {
+case class Arccos[N <: NumericType[M], M](override val argument: NumericTerm[N, M]) extends TrigFunction[N, M] {
   override def inverseConstructorE: Seq[Term] => Either[SimError, Term] =
     mkInverseConstructorE(Cos.apply)
 
   override def newInstance: NewInstanceF = mkNewInstance[Arccos[N,M]](Arccos.apply)
 }
-case class Arctan[N <: NumericType[M], M](override val argument: Term) extends TrigFunction[N, M] {
+case class Arctan[N <: NumericType[M], M](override val argument: NumericTerm[N, M]) extends TrigFunction[N, M] {
   override def inverseConstructorE: Seq[Term] => Either[SimError, Term] =
     mkInverseConstructorE(Tan.apply)
 
