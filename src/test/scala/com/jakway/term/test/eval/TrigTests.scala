2f8513bbac01b49498b151de1b875573c5565253
Thomas Jakway <tjakway@nyu.edu>
2018-11-16 22:19:22 -0800

    added implicit Equality[Term] instance

--- src/test/scala/com/jakway/term/test/eval/TrigTests.scala
+++ src/test/scala/com/jakway/term/test/eval/TrigTests.scala
@@ -1,5 +1,7 @@
 package com.jakway.term.test.eval
 
+import com.jakway.term.elements.Sin
+import com.jakway.term.interpreter.Raw
 import com.jakway.term.numeric.types.NumericType
 import com.jakway.term.test.framework.InterpreterTest
 import org.scalactic.Equality
@@ -9,6 +11,6 @@ class TrigTests[N <: NumericType[M], M]
   extends InterpreterTest[N,M](numericType, equality) {
 
   "eval" should "give sin(0) = 0" in {
-
+    eval(Sin(zero)) shouldEqual zero
   }
 }
