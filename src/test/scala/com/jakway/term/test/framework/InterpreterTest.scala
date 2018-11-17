7abf61ec008d4a730357d847c858bb9ad1d9b978
Thomas Jakway <tjakway@nyu.edu>
2018-11-16 21:49:57 -0800

    added readLiteral to InterpreterTest

--- src/test/scala/com/jakway/term/test/framework/InterpreterTest.scala
+++ src/test/scala/com/jakway/term/test/framework/InterpreterTest.scala
@@ -1,9 +1,25 @@
 package com.jakway.term.test.framework
 
 import com.jakway.term.numeric.types.NumericType
+import com.jakway.term.test.NumericTypeTest
+import com.jakway.term.test.framework.InterpreterTest.ReadLiteralError
 import org.scalactic.Equality
 
 class InterpreterTest[N <: NumericType[M], M]
-  (implicit val equality: Equality[M]) {
+  (val numericType: N, implicit val equality: Equality[M])
+  extends NumericTypeTest[N, M](numericType) {
 
+  //for convenience
+  def readLiteral(x: String): M = {
+    numericType.readLiteral(x) match {
+      case Right(y) => y
+      case Left(err) =>
+        throw ReadLiteralError(s"Error in InterpreterTest.readLiteral: $err")
+    }
+  }
+}
+
+object InterpreterTest {
+  case class ReadLiteralError(override val msg: String)
+    extends TestError(msg)
 }
