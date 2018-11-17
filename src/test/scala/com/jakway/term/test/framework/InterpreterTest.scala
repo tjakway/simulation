daf3f1c55da77737f40570d8eb4e2b5ed9c96bef
Thomas Jakway <tjakway@nyu.edu>
2018-11-16 21:54:37 -0800

    InterpreterTest now extends FlatSpec and Matchers

--- src/test/scala/com/jakway/term/test/framework/InterpreterTest.scala
+++ src/test/scala/com/jakway/term/test/framework/InterpreterTest.scala
@@ -4,10 +4,13 @@ import com.jakway.term.numeric.types.NumericType
 import com.jakway.term.test.NumericTypeTest
 import com.jakway.term.test.framework.InterpreterTest.ReadLiteralError
 import org.scalactic.Equality
+import org.scalatest.{FlatSpec, Matchers}
 
 class InterpreterTest[N <: NumericType[M], M]
   (val numericType: N, implicit val equality: Equality[M])
-  extends NumericTypeTest[N, M] {
+  extends FlatSpec
+    with Matchers
+    with NumericTypeTest[N, M] {
 
   //for convenience
   def readLiteral(x: String): M = {
