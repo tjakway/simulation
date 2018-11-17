39a85d19e07bddc433212d392b9ce8c3650d2cfb
Thomas Jakway <tjakway@nyu.edu>
2018-11-16 22:39:50 -0800

    added trig test instances for Double

--- src/test/scala/com/jakway/term/test/TestInstances.scala
+++ src/test/scala/com/jakway/term/test/TestInstances.scala
@@ -3,26 +3,49 @@ package com.jakway.term.test
 import com.jakway.term.numeric.types.NumericType
 import com.jakway.term.numeric.types.implementations.DoublePrecision
 
-/**
-  * TODO: not sure if these tests should extend NumericTypeTest, or if
-  * NumericTypeTest is even necessary...
-  */
-package double {
+package instances {
+
   object Double {
     type M = Double
     type N = NumericType[Double]
 
     val inst =
       NumericType.Implementations.getDoublePrecisionNumericTypeImplementation()
-      .right.get
+        .right.get
   }
-  import Double._
+  import com.jakway.term.test.framework.ToleranceInstances
 
-  class DoubleTestHasSubterms extends TestHasSubterms[N, M](inst)
-  class DoubleTestFindVariables extends TestFindVariables[N, M](inst)
-  class DoubleTestSubstituteFunctions
-    extends TestSubstituteFunctions[N, M](inst)
+  /**
+    * TODO: not sure if these tests should extend NumericTypeTest, or if
+    * NumericTypeTest is even necessary...
+    */
+  package double {
 
-  class DoubleTestInverseIdentitySimplifier
-    extends TestInverseIdentitySimplifier[N, M](inst)
+
+    import Double._
+
+    class DoubleTestHasSubterms extends TestHasSubterms[N, M](inst)
+
+    class DoubleTestFindVariables extends TestFindVariables[N, M](inst)
+
+    class DoubleTestSubstituteFunctions
+      extends TestSubstituteFunctions[N, M](inst)
+
+    class DoubleTestInverseIdentitySimplifier
+      extends TestInverseIdentitySimplifier[N, M](inst)
+
+  }
+
+  object ExactTests {
+    lazy val exact: ToleranceInstances =
+      ToleranceInstances.getOrThrow("0")
+  }
+
+  package interpreter {
+    import ExactTests._
+    import com.jakway.term.test.eval.TrigTests
+
+    import Double._
+    class DoubleTrigTests extends TrigTests(inst, exact.doubleEquality)
+  }
 }
