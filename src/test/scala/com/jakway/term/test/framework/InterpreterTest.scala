108edcd8f12d7abb96866a6becad1fdd9016f409
Thomas Jakway <tjakway@nyu.edu>
2018-11-24 19:00:32 -0500

    changed InterpreterTest to use a new type for term comparisons

--- src/test/scala/com/jakway/term/test/framework/InterpreterTest.scala
+++ src/test/scala/com/jakway/term/test/framework/InterpreterTest.scala
@@ -58,7 +58,44 @@ abstract class InterpreterTest[N <: NumericType[M], M]
   /**
     * implicit Equality[Term] instance so shouldEqual can be used with Terms
     */
-  implicit val termEquality: Equality[Term] = TermMatchers.equalityInstance
+  implicit val termEqualityObj: Equality[Term] =
+    new NumericTermComp(TermMatchers.equalityInstance)
+
+  /**
+    * if the terms aren't equivalent, try comparing any underlying
+    * numbers using the Equality[M] object
+    * @param termEquality used to compare the terms
+    */
+  class NumericTermComp(val termEquality: Equality[Term]) extends Equality[Term] {
+    override def areEqual(a: Term, b: Any): Boolean = {
+      termEquality.areEqual(a, b) ||
+        (b.isInstanceOf[Term] &&
+          cmpNumericValues(a, b.asInstanceOf[Term]).getOrElse(false))
+    }
+
+    /**
+      * if the terms are both Raw, compare them using
+      * the Equality[M] object
+      * @param a
+      * @param b
+      * @return
+      */
+    def cmpNumericValues(a: Term, b: Term): Option[Boolean] = {
+      //we intentionally don't extract or read literals here since the
+      //interpreter should be doing that
+      def getRaw(x: Term): Option[M] = x match {
+        case y: Raw[N @unchecked, M @unchecked] => Some(y.value)
+        case _ => None
+      }
+
+      for {
+        x <- getRaw(a)
+        y <- getRaw(b)
+      } yield {
+        equality.areEquivalent(x, y)
+      }
+    }
+  }
 }
 
 object InterpreterTest {
