bc3e736bc41246748c89ee87ad85d685b2389d68
Thomas Jakway <tjakway@nyu.edu>
2018-12-06 15:45:08 -0500

    made ComputeValues a trait and moved it to its own package

---
+++ src/main/scala/com/jakway/term/run/ComputeValues.scala
@@ -0,0 +1,45 @@
+package com.jakway.term.run
+
+import com.jakway.term.elements.Term
+import com.jakway.term.interpreter.Interpreter.SymbolTable
+import com.jakway.term.run.SimulationRun.ValueStreams
+
+trait ComputeValues {
+  def toSymbolTables(values: ValueStreams): Stream[SymbolTable]
+}
+
+class Permute extends ComputeValues {
+  import Permute._
+
+  def toSymbolTables(values: ValueStreams): Stream[SymbolTable] = {
+    cartesianProduct[String, Term](values)
+  }
+}
+
+object Permute {
+  def cartesianProduct[A, B](in: Map[A, Traversable[B]]): Stream[Map[A, B]] = {
+    def helper(thisMap: (A, Traversable[B]),
+               remainingMaps: Stream[(A, Traversable[B])]): Stream[Map[A, B]] = {
+      val (thisKey, values) = thisMap
+
+
+      val vStream: Stream[(A, B)] = values.map(v => (thisKey, v))
+        .toStream
+      vStream.flatMap { (q: (A, B)) =>
+        remainingMaps.headOption match {
+          case Some(next) => {
+            helper(next, remainingMaps.drop(1))
+              .map(m => m.updated(q._1, q._2))
+          }
+          case None => Stream(Map(q))
+        }
+      }
+    }
+
+    val inStream = in.toStream
+    inStream.headOption match {
+      case Some(x) => helper(x, inStream.drop(1))
+      case None => Stream()
+    }
+  }
+}
--- src/main/scala/com/jakway/term/run/SimulationRun.scala
+++ src/main/scala/com/jakway/term/run/SimulationRun.scala
@@ -12,50 +12,8 @@ object SimulationRun {
   type ValueStreams = Map[String, Stream[Term]]
 }
 
-abstract class SimulationRun
-                            (inputs: ValueStreams,
+abstract class SimulationRun(inputs: ValueStreams,
                              output: String,
-                             toRun: Solvable)
-
-
-abstract class ComputeValues(val values: ValueStreams) {
-
-  def toSymbolTables(): Stream[SymbolTable]
-}
-
-class Permute(val values: ValueStreams) {
-  import Permute._
-
-  def toSymbolTables(): Stream[SymbolTable] = {
-    cartesianProduct[String, Term](values)
-  }
-}
-
-object Permute {
-  def cartesianProduct[A, B](in: Map[A, Traversable[B]]): Stream[Map[A, B]] = {
-    def helper(thisMap: (A, Traversable[B]),
-               remainingMaps: Stream[(A, Traversable[B])]): Stream[Map[A, B]] = {
-      val (thisKey, values) = thisMap
-
-
-      val vStream: Stream[(A, B)] = values.map(v => (thisKey, v))
-        .toStream
-      vStream.flatMap { (q: (A, B)) =>
-        remainingMaps.headOption match {
-          case Some(next) => {
-            helper(next, remainingMaps.drop(1))
-              .map(m => m.updated(q._1, q._2))
-          }
-          case None => Stream(Map(q))
-        }
-      }
-    }
-
-    val inStream = in.toStream
-    inStream.headOption match {
-      case Some(x) => helper(x, inStream.drop(1))
-      case None => Stream()
-    }
-  }
-}
+                             toRun: Solvable,
+                             computeValues: ComputeValues = new Permute())
 
