28f9989c1ef74d827143bfc8477f0e1f461e5b2c
Thomas Jakway <tjakway@nyu.edu>
2018-12-07 16:49:59 -0500

    fixup! writing scalacheck generators for terms

--- src/test/scala/com/jakway/term/test/framework/ArbitraryTerm.scala
+++ src/test/scala/com/jakway/term/test/framework/ArbitraryTerm.scala
@@ -1,6 +1,7 @@
 package com.jakway.term.test.framework
 
-import com.jakway.term.elements.{NumericTerm, Term, Variable}
+import com.jakway.term.elements._
+import com.jakway.term.interpreter.Raw
 import com.jakway.term.numeric.types.NumericType
 import org.scalacheck.{Arbitrary, Gen}
 
@@ -18,9 +19,31 @@ class ArbitraryTerm[N <: NumericType[M], M]
   def genM: Gen[M] = Gen.numStr.map(
     numericType.readLiteral(_).right.get)
 
+  def genRaw: Gen[Raw[N, M]] = genM.map(Raw(_))
+
   def genNumericTerm: Gen[NumericTerm[N, M]] = ???
 
 
   def genTerm: Gen[Term] = ???
 
+  def genLiteral: Gen[Literal[N, M]] = genM.map(m => Literal(m.toString))
+
+  //add in Negative somewhere
+  def genLeaf: Gen[Term] = Gen.oneOf(genRaw, genLiteral, genVariable)
+
+  def genBranch: Gen[Term] = {
+    val binaryFunctions:
+      Seq[(NumericTerm[N, M], NumericTerm[N, M]) => Term] =
+      Seq(Add.apply, Subtract.apply, Multiply.apply, Divide.apply)
+
+    def genBinaryTerm = for {
+      left <- genNumericTerm
+      right <- genNumericTerm
+      f <- Gen.oneOf(binaryFunctions)
+    } yield {
+      f(left, right)
+    }
+  }
+
+  def genNegative: Gen[Negative[N, M]] = genNumericTerm.map(Negative.apply)
 }
