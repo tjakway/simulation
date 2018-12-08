db3ea7454d96b1ee4f91eb8e76d8a4b32a7b35e6
Thomas Jakway <tjakway@nyu.edu>
2018-12-08 17:00:29 -0500

    HasInterpreter now gives better error messages

--- src/test/scala/com/jakway/term/test/framework/gen/HasInterpreter.scala
+++ src/test/scala/com/jakway/term/test/framework/gen/HasInterpreter.scala
@@ -2,9 +2,27 @@ package com.jakway.term.test.framework.gen
 
 import com.jakway.term.interpreter.{Eval, Interpreter}
 import com.jakway.term.numeric.types.NumericType
+import com.jakway.term.test.framework.gen.HasInterpreter.{EvalInitializationError, NullNumericTypeError}
 
 trait HasInterpreter[N <: NumericType[M], M]
   extends HasNumericType[N, M] {
 
-  lazy val interpreter: Interpreter = Eval[N, M](numericType).right.get
+  lazy val interpreter: Interpreter = {
+    if(numericType == null) {
+      throw NullNumericTypeError
+    } else {
+      Eval[N, M](numericType) match {
+        case Right(x) => x
+        case Left(e) => throw EvalInitializationError(e.toString)
+      }
+    }
+  }
+}
+
+object HasInterpreter {
+  case object NullNumericTypeError
+    extends GenError("numericType from HasNumericType was null")
+
+  case class EvalInitializationError(val errMsg: String)
+    extends GenError("Eval.apply returned " + errMsg)
 }
