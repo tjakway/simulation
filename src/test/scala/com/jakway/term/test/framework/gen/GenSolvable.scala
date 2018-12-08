11094d10f229d812a6cd97cbbbe9c1936269bde0
Thomas Jakway <tjakway@nyu.edu>
2018-12-08 11:04:59 -0500

    added GenSolvable

---
+++ src/test/scala/com/jakway/term/test/framework/gen/GenSolvable.scala
@@ -0,0 +1,19 @@
+package com.jakway.term.test.framework.gen
+
+import com.jakway.term.numeric.types.NumericType
+import com.jakway.term.solver.Solvable
+import org.scalacheck.Gen
+
+class GenSolvable[N <: NumericType[M], M]
+  (val numericType: N) {
+
+  private val genTerm: GenTerm[N, M] = new GenTerm[N, M](numericType)
+
+  def genSolvable: Gen[Solvable] = {
+    for {
+      left <- genTerm.genNumericTerm
+      right <- genTerm.genNumericTerm
+    } yield Solvable(left, right)
+  }
+}
+
