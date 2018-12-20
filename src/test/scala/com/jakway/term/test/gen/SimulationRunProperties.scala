04e93837b4a521362d275296571764cb1ccd6278
Thomas Jakway <tjakway@nyu.edu>
2018-12-20 15:46:40 -0800

    added checks to beginning of property execution

--- src/test/scala/com/jakway/term/test/gen/SimulationRunProperties.scala
+++ src/test/scala/com/jakway/term/test/gen/SimulationRunProperties.scala
@@ -1,5 +1,6 @@
 package com.jakway.term.test.gen
 
+import com.jakway.term.interpreter.Interpreter
 import com.jakway.term.numeric.errors.SimError
 import com.jakway.term.numeric.types.NumericType
 import com.jakway.term.run.SimulationRun
@@ -15,12 +16,15 @@ trait SimulationRunProperties[N <: NumericType[M], M]
     with GenSimulationRun[N, M]
     with GenEval[N, M] {
   this: Properties =>
+  import SimulationRunProperties._
 
-  def getGenSimulationRun(): Gen[SimulationRun] =
-    genSimulationRun(false) match {
+  def getGenSimulationRun(): Gen[SimulationRun] = {
+    val res = genSimulationRun(false) match {
       case Right(x) => x
       case Left(e) => throw GenSimulationRunError(e)
     }
+    res.filter(_ != null)
+  }
 
   implicit val arbSimulationRun: Arbitrary[SimulationRun] =
     Arbitrary(getGenSimulationRun())
@@ -28,12 +32,42 @@ trait SimulationRunProperties[N <: NumericType[M], M]
   implicit val ec: ExecutionContext = scala.concurrent.ExecutionContext.global
   property("execute SimulationRun without error") =
     forAll { (simulationRun: SimulationRun) =>
+      assert(simulationRun != null)
+      checkNumericType[N, M](numericType)
+      checkInterpreter(interpreter)
+
       SimulationRun.run(simulationRun, interpreter).isRight
   }
 
 }
 
 object SimulationRunProperties {
+  class SimulationRunPropertiesError(override val msg: String)
+    extends GenError(msg) {
+    def this(t: Throwable) {
+      this(t.toString)
+    }
+  }
+
   case class GenSimulationRunError(e: SimError)
-    extends GenError(s"Error in genSimulationRun: $e")
+    extends SimulationRunPropertiesError(s"Error in genSimulationRun: $e")
+
+  case object NullNumericTypeError
+    extends SimulationRunPropertiesError(
+      "numericType was null at beginning of property execution")
+
+  case object NullInterpreterError
+    extends SimulationRunPropertiesError(
+      "interpreter was null at beginning of property execution")
+
+  def checkNumericType[N <: NumericType[M], M](n: N): Unit =
+    if(n == null) {
+      throw NullNumericTypeError
+    } else {}
+
+  def checkInterpreter(interpreter: Interpreter): Unit = {
+    if(interpreter == null) {
+      throw NullInterpreterError
+    } else {}
+  }
 }
